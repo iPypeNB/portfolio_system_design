81b3ee24261942b0b005b08a70aee3ae3f3f17bc
Felipe Díaz <a.felipediazr00@gmail.com>
2024-12-10 22:13:40 -0500

    feat: Atomic design
    se agrega estructura de atomic design

--- lib/portfolio_system_design.dart
+++ lib/portfolio_system_design.dart
@@ -1,7 +1,16 @@
 library portfolio_system_design;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// foundations
+export 'package:portfolio_system_design/foundations/foundations.dart';
+
+// atoms
+export 'package:portfolio_system_design/atoms/atoms.dart';
+
+// molecules
+export 'package:portfolio_system_design/molecules/molecules.dart';
+
+// organisms
+export 'package:portfolio_system_design/organisms/organisms.dart';
+
+// utils
+export 'package:portfolio_system_design/utils/utils.dart';
